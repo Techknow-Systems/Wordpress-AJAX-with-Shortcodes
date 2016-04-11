b40e8ecccddfa7eaa06e867c02375a329928c736
Shaa Taylor <shaa@techknowsystems.com.au>
2016-04-11 16:09:47 +1000

    Update jquery.make-ajax-call.js

--- jquery.make-ajax-call.js
+++ jquery.make-ajax-call.js
@@ -1,50 +1,48 @@
 jQuery(function($){
 
   // The page variable here allows us to page data that comes back fromthe AJAX call
-	var page = 2;
-	var loading = false;
+  var page = 2;
+  var loading = false;
 
-	$('body').on('click', '.ajax-button', function() {  // Activate AJAX call on button click
+  $('body').on('click', '.ajax-button', function() {  // Activate AJAX call on button click
 
-		if (!loading ) {
+    If (!loading ) {
 		
-			loading = true;
+      loading = true;
 
       // Set up the data to call my-ajax-call.php with
-			var data = {
-				nonce: ajaxcall.nonce,
-				page: page,
-				query: ajaxcall.query,
-			};
-			$.post(ajaxcall.url, data, function(result) {
+      var data = {
+	nonce: ajaxcall.nonce,
+	page: page,
+	query: ajaxcall.query,
+      };
+      $.post(ajaxcall.url, data, function(result) {
 		
-				if( result.success) {
+        if( result.success) {
 
           // Output the info returned in "result.data" from my-ajax-call.php here
-          //
           // Example:
-          // 
           // $('.ajax-results').append( result.data );
 
-					page = page + 1;
-					loading = false;
+	  page = page + 1;
+	  loading = false;
 
-				} else {
+        } else {
 
-           // If the AJAX call failed, output the reason to the console
-					 console.log(result);
+      	  // If the AJAX call failed, output the reason to the console
+	  console.log(result);
 
-				}
+        }
 
-			}).fail(function(xhr, textStatus, e) {
+      }).fail(function(xhr, textStatus, e) {
 
         // If the AJAX call failed, output the reason to the console
-				console.log(xhr.responseText);
+        console.log(xhr.responseText);
 
-			});
+      });
 
-		}
-
-	});
+    }
+  
+  });
 		
 }); 
