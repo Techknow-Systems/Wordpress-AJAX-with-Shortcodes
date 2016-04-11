d99d2167f9b209686e134688064a7eaff5a8f11d
Shaa Taylor <shaa@techknowsystems.com.au>
2016-04-11 16:11:15 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,20 +1,24 @@
-add_action( 'wp_enqueue_scripts', 'enqueue_ajax_call_js' );
-function enqueue_ajax_call_js() {
+<?php
+
+  add_action( 'wp_enqueue_scripts', 'enqueue_ajax_call_js' );
+  function enqueue_ajax_call_js() {
   
-    // The info we meed to build the WP_Query Object
-    $query = array( 
-    	// Set up your WP_Query arguements here 
-    );
+      // The info we meed to build the WP_Query Object
+      $query = array( 
+    	  // Set up your WP_Query arguements here 
+      );
 
-    // Info we need for the AJAX Call. 
-    $args = array(
-    	'nonce' => wp_create_nonce('ajax-nonce'),
-    	'url'   => '/link.to.your/my-ajax-call.php',
-    	'query' => $query,
-  	);
+      // Info we need for the AJAX Call. 
+      $args = array(
+    	  'nonce' => wp_create_nonce('ajax-nonce'),
+    	  'url'   => '/link.to.your/my-ajax-call.php',
+    	  'query' => $query,
+  	  );
 
-    // Enquue the jQuery and locallize it
-    wp_enqueue_script('ajax-call', '/link.to.your/js/jquery.make-ajax-call.js', array( 'jquery' ), '0.1', true);
-    wp_localize_script('ajax-call', 'ajaxcall', $args);
+      // Enquue the jQuery and locallize it
+      wp_enqueue_script('ajax-call', '/link.to.your/js/jquery.make-ajax-call.js', array( 'jquery' ), '0.1', true);
+      wp_localize_script('ajax-call', 'ajaxcall', $args);
 
-}
+  }
+  
+?>
